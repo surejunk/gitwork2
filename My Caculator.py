6b68691023bb1f6775d7f75faf321a36e2735296
surejunk <surevunk@gmail.com>
2017-11-06 12:26:27 +0800

    Update My Caculator.py

--- My Caculator.py
+++ My Caculator.py
@@ -5,7 +5,6 @@ class Calculator(QWidget):
     def __init__(self,parent=None):
         super().__init__(parent)
         self.initUI()
-#----------------------------------------------------------
     def initUI(self):
         grid = QGridLayout()
         self.setLayout(grid)
@@ -28,7 +27,6 @@ class Calculator(QWidget):
 
     def buttonClicked(self):
         sender = self.sender();
-        # self.display.setText(sender.text())
         key = sender.text()
         if key == "=":
             result = str(eval(self.display.text()))
